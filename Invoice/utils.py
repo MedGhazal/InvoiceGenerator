aa14081eed5b0cd107c05565823d272557c89b61
Mohamed Ghazal <medghazalcs@gmail.com>
2024-11-30 18:53:12 +0100

    Remove bug of payments

--- Invoice/utils.py
+++ Invoice/utils.py
@@ -143,7 +143,7 @@ def generate_invoice_tex(invoice):
     activities, invoiceStatus, invoiceType = parse_activities(invoice)
     if invoice.draft:
         invoiceBlock = f'''
-Date d{invoiceStatus}: {date.today().year}-{invoice.count}\\\\
+Devis: {date.today().year}-{invoice.count}\\\\
         '''
     else:
         invoiceBlock = f'''
