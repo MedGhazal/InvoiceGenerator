305b5602255c239c45086144b59f090df58b3b9b
Mohamed Ghazal <medghazalcs@gmail.com>
2024-12-05 14:18:13 +0100

    Add first plot to the dashboad

--- home/views.py
+++ home/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+
+from Invoice.models import Invoice
+
+from bokeh.plotting import figure, show
+from bokeh.document import Document
+from bokeh.layouts import column
+from bokeh.models import Slider
+from bokeh.embed import server_document, components
+
 from .forms import ContactDataForm
 
 
 @login_required()
 def index(request):
-    return render(request, 'home-index.html', {})
+    # script = create_dashboard(request.build_absolute_uri())
+    x = [invoice.id for invoice in Invoice.objects.all()]
+    y = [float(invoice.owedAmount) for invoice in Invoice.objects.all()]
+    z = [float(invoice.paidAmount) for invoice in Invoice.objects.all()]
+    dashboard = figure(
+        title=_('DASHBOAD'),
+        x_axis_label=_('TEXT'),
+        y_axis_label=_('TEXT'),
+        width=300,
+        height=300,
+    )
+    dashboard.line(x, y, line_width=2)
+    dashboard.line(x, z, line_width=2)
+    scripts, dashboard = components(dashboard)
+    context = {
+        'scripts': scripts,
+        'dashboard': dashboard,
+    }
+    return render(request, 'home-index.html', context)
 
 
 def register_success(request):
@@ -25,3 +53,8 @@ def register_user(request):
         'registration/register.html',
         {'form': userCreationForm},
     )
+
+
+def create_dashboard(document) -> None:
+    slider = Slider(start=0, end=30, value=0, step=1, title="Example")
+    document.add_root(column(slider))
